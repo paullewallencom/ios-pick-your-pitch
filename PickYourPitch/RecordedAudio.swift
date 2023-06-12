fb3c04ec1425e4e45b1382c79afd769a8e110b08
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-06-11 20:58:39 -0700

    Updated Files
    Updated files.

---
+++ PickYourPitch/RecordedAudio.swift
@@ -0,0 +1,19 @@
+//
+//  RecordedAudio.swift
+//  PickYourPitch
+//
+//  Created by Apple Computer on 6/11/23.
+//
+
+import Foundation
+
+// MARK: - RecordedAudio
+
+struct RecordedAudio {
+    
+    // MARK: Properties
+    
+    var filePathUrl: URL!
+    var title: String!
+    
+}
